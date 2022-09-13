b0ef8fae524471157553882404ca4b5d1fbed499
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-09-13 17:12:44 +0200

    make appointment  section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 window.onscroll = function() {myFunction()};
 
-// function myFunction() {
-//   console.log(document.documentElement.scrollTop);
-//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-//     document.getElementsByClassName("nav")[0].style.top=0;
-//   }
-//   else {document.getElementsByClassName("nav")[0].style.top=null}
-
-// }
 
 function myFunction() {
   let scrollValue=(document.getElementsByClassName("nav")[0]).offsetTop;
